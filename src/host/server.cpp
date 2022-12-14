e85bcf7d2f8f738c94b6d91e6fb19790de19f770
leiguangyu <leiguangyu@huawei.com>
2022-12-14 11:01:13 +0800

    Add log for jdwp
    Change-Id: If30eeed964665c8fdd5f455658db9dde2f6fc1e9
    Signed-off-by: leiguangyu <leiguangyu@huawei.com>

--- src/host/server.cpp
+++ src/host/server.cpp
@@ -563,7 +563,7 @@ bool HdcServer::FetchCommand(HSession hSession, const uint32_t channelId, const 
             MessageLevel level = static_cast<MessageLevel>(*payload);
             string s(reinterpret_cast<char *>(payload + 1), payloadSize - 1);
             sfc->EchoClient(hChannel, level, s.c_str());
-            WRITE_LOG(LOG_DEBUG, "CMD_KERNEL_ECHO size:%d", payloadSize - 1);
+            WRITE_LOG(LOG_INFO, "CMD_KERNEL_ECHO size:%d channelId:%u", payloadSize - 1, channelId);
             break;
         }
         case CMD_KERNEL_CHANNEL_CLOSE: {
