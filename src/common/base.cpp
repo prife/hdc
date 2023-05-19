863015e8bc88d3861fb566fa210cb3c846f97bbd
zhaolihui <zhaolihui2@huawei.com>
2023-05-19 17:57:05 +0800

    Add close fd test log
    Signed-off-by: zhaolihui <zhaolihui2@huawei.com>

--- src/common/base.cpp
+++ src/common/base.cpp
@@ -1631,6 +1631,7 @@ namespace Base {
     int CloseFd(int &fd)
     {
         int rc = 0;
+        WRITE_LOG(LOG_INFO, "CloseFd fd:%d", fd);
         if (fd > 0) {
             rc = close(fd);
             if (rc < 0) {
