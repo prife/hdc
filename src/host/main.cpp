6ba3e0c834a799a039d74e3f634fe4f98c4e4dbb
faithwang <wangyanteng@huawei.com>
2023-06-06 22:30:59 +0800

    update getopt
    Signed-off-by: faithwang <wangyanteng@huawei.com>

--- src/host/main.cpp
+++ src/host/main.cpp
@@ -291,7 +291,7 @@ bool GetCommandlineOptions(int optArgc, const char *optArgv[])
     bool needExit = false;
     opterr = 0;
     // get option parameters first
-    while ((ch = getopt(optArgc, const_cast<char *const*>(optArgv), "h:vpfmncs:Sd:t:l:")) != -1) {
+    while ((ch = getopt(optArgc, const_cast<char *const*>(optArgv), "hvpfmncs:Sd:t:l:")) != -1) {
         switch (ch) {
             case 'h': {
                 string usage = Hdc::TranslateCommand::Usage();
