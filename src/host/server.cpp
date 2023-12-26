ee59af6245bc452a2e5251be54e5016d5a8add8d
liuwei <liuwei573@huawei.com>
2023-12-26 19:49:04 +0800

    hdc list targets bugfix
    Signed-off-by: liuwei <liuwei573@huawei.com>

--- src/host/server.cpp
+++ src/host/server.cpp
@@ -700,7 +700,6 @@ void HdcServer::UsbPreConnect(uv_timer_t *handle)
     HSession hSession = (HSession)handle->data;
     bool stopLoop = false;
     HdcServer *hdcServer = (HdcServer *)hSession->classInstance;
-    const int usbConnectRetryMax = 5;
     while (true) {
         WRITE_LOG(LOG_DEBUG, "HdcServer::UsbPreConnect");
         HDaemonInfo pDi = nullptr;
