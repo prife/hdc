b5191b3c8cde272b671f371ede912aa7f2f1a806
liupeng298@huawei.com <liupeng298@huawei.com>
2024-05-21 14:38:22 +0800

    1135影响daemon侧文件传输速率，还原，只修改host usb部分
    Signed-off-by: liupeng298@huawei.com <liupeng298@huawei.com>

--- hdc_rust/src/transfer/base.rs
+++ hdc_rust/src/transfer/base.rs
@@ -164,7 +164,7 @@ pub async fn unpack_task_message_lock(
     }
 }
 
-pub async fn unpack_task_message(
+pub fn unpack_task_message(
     rd: &mut dyn Reader,
     tx: BoundedSender<(TaskMessage, u32)>,
 ) -> io::Result<()> {
@@ -174,47 +174,50 @@ pub async fn unpack_task_message(
     }
 
     let data = rd.read_frame(pack_size as usize)?;
-    let (head, body) = data.split_at(serializer::HEAD_SIZE);
-    let payload_head = serializer::unpack_payload_head(head.to_vec())?;
-    let expected_head_size = u16::from_be(payload_head.head_size) as usize;
-    let expected_data_size = u32::from_be(payload_head.data_size) as usize;
+    ylong_runtime::spawn(async move {
+        let (head, body) = data.split_at(serializer::HEAD_SIZE);
+        let payload_head = serializer::unpack_payload_head(head.to_vec())?;
+        let expected_head_size = u16::from_be(payload_head.head_size) as usize;
+        let expected_data_size = u32::from_be(payload_head.data_size) as usize;
 
-    if serializer::HEAD_SIZE + expected_head_size + expected_data_size != pack_size as usize {
-        crate::warn!(
-            "protocol size diff: {pack_size} != {} + {expected_head_size} + {expected_data_size}",
-            serializer::HEAD_SIZE
-        );
-    }
-
-    if expected_head_size + expected_data_size == 0
-        || expected_head_size + expected_data_size > HDC_BUF_MAX_SIZE
-    {
-        return Err(Error::new(ErrorKind::Other, "Packet size incorrect"));
-    }
-
-    let (protect, payload_raw) = body.split_at(expected_head_size);
-    let (payload, _) = payload_raw.split_at(expected_data_size);
-
-    let payload_protect = serializer::unpack_payload_protect(protect.to_vec())?;
-    let channel_id = payload_protect.channel_id;
-
-    let command = match HdcCommand::try_from(payload_protect.command_flag) {
-        Ok(command) => command,
-        Err(_) => {
-            return Err(Error::new(ErrorKind::Other, "unknown command"));
+        if serializer::HEAD_SIZE + expected_head_size + expected_data_size != pack_size as usize {
+            crate::warn!(
+                "protocol size diff: {pack_size} != {} + {expected_head_size} + {expected_data_size}",
+                serializer::HEAD_SIZE
+            );
         }
-    };
 
-    let _ = tx
-        .send((
-            TaskMessage {
-                channel_id,
-                command,
-                payload: payload.to_vec(),
-            },
-            package_index,
-        ))
-        .await;
+        if expected_head_size + expected_data_size == 0
+            || expected_head_size + expected_data_size > HDC_BUF_MAX_SIZE
+        {
+            return Err(Error::new(ErrorKind::Other, "Packet size incorrect"));
+        }
+
+        let (protect, payload_raw) = body.split_at(expected_head_size);
+        let (payload, _) = payload_raw.split_at(expected_data_size);
+
+        let payload_protect = serializer::unpack_payload_protect(protect.to_vec())?;
+        let channel_id = payload_protect.channel_id;
+
+        let command = match HdcCommand::try_from(payload_protect.command_flag) {
+            Ok(command) => command,
+            Err(_) => {
+                return Err(Error::new(ErrorKind::Other, "unknown command"));
+            }
+        };
+
+        let _ = tx
+            .send((
+                TaskMessage {
+                    channel_id,
+                    command,
+                    payload: payload.to_vec(),
+                },
+                package_index,
+            ))
+            .await;
+        Ok(())
+    });
 
     Ok(())
 }
